2ab50b6eaed0193e6bb98f2cce4f2d166d7db2f2
ampp8800 <87927435+ampp8800@users.noreply.github.com>
2023-04-21 23:18:57 +0300

    adding authorization and registration functionality

--- app/src/main/java/com/example/health/MainRegisterActivity.kt
+++ app/src/main/java/com/example/health/MainRegisterActivity.kt
@@ -1,46 +1,79 @@
 package com.example.health
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import de.hdodenhof.circleimageview.CircleImageView
 
 class MainRegisterActivity : AppCompatActivity() {
     lateinit var editLogin: EditText //называние с маленькой буквы
     lateinit var editPasswordFirst: EditText
     lateinit var editPasswordSecond: EditText
-    lateinit var buttonUp: ViewGroup
+    lateinit var signUp: View
     lateinit var avatar: CircleImageView
+    lateinit var repository: Repository
+    lateinit var urlImage: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_regist)
         init()
-        buttonUp.setOnClickListener {
+        signUp.setOnClickListener {
             addNewLogin()
+        }
+        avatar.setOnClickListener {
+            addAvatar()
         }
     }
 
     private fun init() {
+        repository = Repository.getInstance()
         editLogin = findViewById(R.id.nickname_input)
         editPasswordFirst = findViewById(R.id.et_password_first)
-        buttonUp = findViewById<ViewGroup>(R.id.b_sign_in)
         editPasswordSecond = findViewById(R.id.et_password_second)
         avatar = findViewById(R.id.image_user)
+        signUp = findViewById(R.id.sign_up_btn)
     }
 
     private fun addNewLogin() {
-        var passwordFirst: String = editPasswordFirst.text.toString()
-        var passwordSecond: String = editPasswordSecond.text.toString()
-        var login: String = editLogin.text.toString()
-        if (passwordFirst.equals(passwordSecond)) {
-
+        if (editPasswordFirst.text.toString().equals(editPasswordSecond.text.toString())){
+            repository.password = editPasswordFirst.text.toString()
+            repository.userName = editLogin.text.toString()
+            repository.imageUrl = urlImage
+            val intent = Intent(this, AuthorizationActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            Toast.makeText(this, R.string.wrong_registration, Toast.LENGTH_SHORT).show()
         }
+    }
 
+    private fun addAvatar() {
+        val layoutInflater = LayoutInflater.from(this)
+        val pompt = layoutInflater.inflate(R.layout.fast_text_entry, null)
+        val dialogBilder = AlertDialog.Builder(this)
+        dialogBilder.setView(pompt)
+        val userInput : EditText = pompt.findViewById(R.id.input_text)
+        val negativeButton = dialogBilder.setCancelable(false)
+            .setPositiveButton("OK") { dialog, id ->
+                urlImage = userInput.text.toString()
+                Glide.with(this)
+                    .load(urlImage)
+                    .into(avatar)
+            }
+            .setNegativeButton("Отмена") { dialog, id ->
+                dialog.cancel()
+            }
+        dialogBilder.create().show()
     }
 
 }
